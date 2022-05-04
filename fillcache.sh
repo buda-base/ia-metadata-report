26d68aecc922913cf2295187170d4afa16288260
eroux <elie.roux@telecom-bretagne.eu>
2022-05-04 10:31:30 +0200

    fix query limit and regenerate report

--- fillcache.sh
+++ fillcache.sh
@@ -5,7 +5,7 @@ ia search 'collection:bdrc-fplmanuscripts' | sed -re 's/^\{"identifier": "bdrc-(
 ia search 'collection:bdrc-khmermanuscripts' | sed -re 's/^\{"identifier": "bdrc-(.*)"\}$/\1/g' | sort > cache/ia-bdrc-khmermanuscripts.txt
 ia search 'collection:buddhist-digital-resource-center AND collection:stream_only' | sed -re 's/^\{"identifier": "bdrc-(.*)"\}$/\1/g' | sort > cache/ia-bdrc-stream_only.txt
 ia search 'collection:buddhist-digital-resource-center AND collection:buddhist-digital-resource-center-restricted' | sed -re 's/^\{"identifier": "bdrc-(.*)"\}$/\1/g' | sort > cache/ia-bdrc-restricted.txt
-curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeunlisted?format=csv&pageSize=8000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeunlinsted.txt
-curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeingeorestricted?format=csv&pageSize=8000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeingeorestricted.txt
-curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeinlibrary?format=csv&pageSize=8000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeinlibrary.txt
-curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeinbdrcrestricted?format=csv&pageSize=8000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeinbdrcrestricted.txt
+curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeunlisted?format=csv&pageSize=20000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeunlinsted.txt
+curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeingeorestricted?format=csv&pageSize=20000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeingeorestricted.txt
+curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeinlibrary?format=csv&pageSize=20000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeinlibrary.txt
+curl 'https://ldspdi.bdrc.io/query/table/IA_mustbeinbdrcrestricted?format=csv&pageSize=20000' | tail -n +2 | sed -re 's/^"bdr:(.*)"$/\1/g' | sort > cache/bdrc-mustbeinbdrcrestricted.txt
