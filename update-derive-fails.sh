e2f75b29ae5888394fb34f984a0daaafcb587a95
Jim Katz <jimk@tbrc.org>
2022-07-11 11:56:49 -0400

    Slimmed down file list. Still generates false positives

--- update-derive-fails.sh
+++ update-derive-fails.sh
@@ -4,4 +4,4 @@
 # You have to be TimB-BDRC in ia to get meaningful data
 # Run this sparingly
 ftnow=$(date +%Y-%m-%d_%H-%M-%S)
-ia tasks --red-rows | cut -f1 | sed -re 's/bdrc-(.*)/\1/g' | sort > "${ftnow}".derivefailed.lst
+ia tasks --red-rows | cut -f1 | sed -re 's/bdrc-(.*)/\1/g' | sort > "${ftnow}".derivedfailed.lst
